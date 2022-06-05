1b030f28e055b4f12b8fc311a8cfb02926708837
Dan-Matei Mitreanu <dan@mitreanu.ro>
2022-06-05 18:27:13 +0300

    Type Initialization Fail Example

---
+++ Examples/ExampleTypeFail/Program.cs
@@ -0,0 +1,38 @@
+﻿using esuite.Ditester;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+var ditester = new Ditester((_, services) =>
+{
+    //services.AddSingleton<NonInjectedService>();
+});
+
+ditester.StartAndRun();
+
+foreach (var tr in ditester.GetResults())
+{
+    Console.WriteLine(tr);
+    if (!tr.Success)
+        Console.WriteLine($"Exception:\n{tr.Exception?.Message}\n{tr.Exception?.InnerException?.Message}");
+}
+
+// Woops, I forgot to add this service to the IServiceCollection,
+// I wonder what might happen...?
+public class NonInjectedService
+{
+    private ILogger _logger;
+
+    public NonInjectedService(ILogger logger) => _logger = logger;
+
+    public void Msg1() => _logger.LogInformation("message 1");
+}
+
+public class TestClass : IDitest
+{
+    private NonInjectedService _nis;
+
+    public TestClass(NonInjectedService nis) => _nis = nis;
+
+    public void Test1() => _nis.Msg1();
+}
